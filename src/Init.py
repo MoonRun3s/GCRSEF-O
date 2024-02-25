0780081c37b3d345c6109cc19ecf64101a5318d1
jrich <68448908+MoonRun3s@users.noreply.github.com>
2024-02-24 22:32:37 -0500

    Update and rename Init.py to src/Init.py

--- src/Init.py
+++ src/Init.py
@@ -8,3 +8,12 @@ import board
 from adafruit_motorkit import MotorKit
 
 kit = MotorKit(i2c=board.I2C())
+kit.motor1.throttle = 1.0
+kit.motor2.throttle = 1.0
+kit.motor3.throttle = 1.0
+kit.motor4.throttle = 1.0
+
+### or...
+
+kit.stepper1.onestep()
+kit.stepper2.onestep()
